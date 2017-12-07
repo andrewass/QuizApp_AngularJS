b133a6af95c9bf6f48fec020010698b7183ebd10
andrewass <a.wassum@gmail.com>
2017-12-07 22:50:13 +0100

    added score

--- js/questionController.js
+++ js/questionController.js
@@ -4,6 +4,9 @@ mainModule.controller("questionCtrl", function ($scope) {
 
     $scope.ind = 0;
     $scope.selectedAnswer = undefined;
+    $scope.alertMsg = "";
+    $scope.correctAnswered = 0;
+    $scope.answered = 0;
 
     /* Increment the question index to display the next question */
     $scope.getNextQuestion = function () {
@@ -17,17 +20,24 @@ mainModule.controller("questionCtrl", function ($scope) {
     /* Terminates the question mode, and prepare for the main menu view to be displayed */
     $scope.exitQuestionMode = function () {
         $scope.ind = 0;
+        $scope.answered = 0;
+        $scope.correctAnswered = 0;
+        $scope.alertMsg = "";
         $scope.selectedAnswer = undefined;
         return true;
     }
 
 
     /* Check if the selected answer matches correct answer */
-    $scope.checkAnswer = function (answered) {
+    $scope.checkAnswer = function (answered, correctAlt) {
         if($scope.selectedAnswer !== undefined){
             return;
         }
         $scope.selectedAnswer = answered;
+        $scope.answered++;
+        if(correctAlt === answered){
+            $scope.correctAnswered++;
+        }
     }
 
 
